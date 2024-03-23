fc77057bce777adf7ea85ea3e0379543270961cd
Saurabh Tyagi <personal.saurabhtyagi@hotmail.com>
2024-03-23 12:18:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Experience from "./pages/Experience";
-import Projects from "./pages/Projects";
+// import Projects from "./pages/Projects";
 
 function App() {
   return (
